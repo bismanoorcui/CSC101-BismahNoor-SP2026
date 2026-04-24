b17b114eb5485be95d3095da4cce81601eb98025
bismanoorcui <bismahnoor355@gmail.com>
2026-04-24 20:56:03 +0500

    assignment.py

---
+++ assignment.py
@@ -0,0 +1,12 @@
+Name: Bisma Noor 
+Registration Number: SP26-ELC-021
+Section: ELC 1A
+
+Why learning programming is important for me:
+
+Programming is important for me because it helps me develop strong problem solving and logical thinking skills.
+It allows me to understand how software and technology work in the real world.
+By learning programming, I can create my own applications and projects in the future.
+It also opens many career opportunities in fields related to technology and innovation.
+Programming improves my creativity and helps me think in a structured and efficient way.
+I believe it is an essential skill for success in today’s digital world.
